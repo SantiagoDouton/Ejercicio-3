454924adf5dbb56ca5949823d350198c5d535d70
SantiagoDouton <douton.santiago@gmail.com>
2022-06-24 22:29:45 -0300

    Ejercicio 3

---
+++ Js/script.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Introduzca su nombre");
+let edad = prompt("Introduzca su edad");
+let pasadoAño = edad - 1;
+
+alert (nombre + " el pasado año tenías " + pasadoAño + " y el próximo año cumplirías " + (+edad + + 1));
